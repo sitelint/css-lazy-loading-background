4a9932916559104780916bb69139732730ade756
Cezary Tomczyk <115872268+sitelint@users.noreply.github.com>
2023-06-19 10:14:33 +0200

    Refactoring and replace document.querySelectorAll('[style*=\'--background-image-lazy\']')

--- dist/css-lazy-loading-background.d.ts
+++ dist/css-lazy-loading-background.d.ts
@@ -1,7 +1,15 @@
 declare class CssLazyLoadingBackground {
     private lazyBackgroundObserver;
     private isIntersectionObserverSupported;
+    private processEntriesBindInstance;
     constructor();
+    private loadBackgroundIfElementOnScreen;
+    private processEntries;
+    private observeElementVisibility;
+    private setBackground;
+    private getLazyBackgroundDetail;
+    private initialiseStyleBackgroundIntersectionObserver;
+    destroy(): void;
     initialise(): void;
 }
 
--- src/css-lazy-loading-background.ts
+++ src/css-lazy-loading-background.ts
@@ -4,68 +4,94 @@ import { CommonUtilities } from './utilities/common.utilities';
 export class CssLazyLoadingBackground {
   private lazyBackgroundObserver: IntersectionObserver | null;
   private isIntersectionObserverSupported: boolean;
+  private processEntriesBindInstance: any;
 
   constructor() {
     this.lazyBackgroundObserver = null;
     this.isIntersectionObserverSupported = CommonUtilities.isHostMethod(globalThis, 'IntersectionObserver');
+    this.processEntriesBindInstance = null;
+  }
+
+  private loadBackgroundIfElementOnScreen(entry: IntersectionObserverEntry): void {
+    if (entry.isIntersecting === false) {
+      return;
+    }
+
+    const target: HTMLElement = (entry.target as HTMLElement);
+    const backgroundImageLazy: string = globalThis.getComputedStyle(target).getPropertyValue('--background-image-lazy');
+
+    if (backgroundImageLazy.length === 0) {
+      return;
+    }
+
+    target.style.backgroundImage = `url('${backgroundImageLazy.replace(/(?:\\(.))/g, '$1').trim()}')`;
+    target.style.removeProperty('--background-image-lazy');
+    this.lazyBackgroundObserver!.unobserve(target);
+  }
+
+  private processEntries(entries: IntersectionObserverEntry[]): void {
+    for (const entry of entries) {
+      this.loadBackgroundIfElementOnScreen(entry);
+    }
+  }
+
+  private observeElementVisibility(lazyBackground: Element): void {
+    this.lazyBackgroundObserver!.observe(lazyBackground);
+  }
+
+  private setBackground(element: Element): void {
+    const backgroundImageLazy = globalThis.getComputedStyle(element).getPropertyValue('--background-image-lazy');
+
+    if (backgroundImageLazy.length === 0) {
+      return;
+    }
+
+    (element as HTMLElement).style.backgroundImage = `url('${backgroundImageLazy.replace(/(?:\\(.))/g, '$1').trim()}')`;
+    (element as HTMLElement).style.removeProperty('--background-image-lazy');
+  }
+
+  private getLazyBackgroundDetail(element: Element): boolean {
+    return globalThis.getComputedStyle(element).getPropertyValue('--background-image-lazy').length > 0;
+  }
+
+  private initialiseStyleBackgroundIntersectionObserver(): void {
+    const allElements: Element[] = Array.from(document.querySelectorAll('body *'));
+
+    if (allElements.length === 0) {
+      return;
+    }
+
+    const elementsWithLazyBackground: Element[] = allElements.filter(this.getLazyBackgroundDetail);
+
+    if (elementsWithLazyBackground.length === 0) {
+      return;
+    }
+
+    if (this.isIntersectionObserverSupported) {
+      this.processEntriesBindInstance = this.processEntries.bind(this);
+      this.lazyBackgroundObserver = new IntersectionObserver(this.processEntriesBindInstance);
+
+      for (const element of elementsWithLazyBackground) {
+        this.observeElementVisibility(element);
+      }
+
+      return;
+    }
+
+    for (const element of elementsWithLazyBackground) {
+      this.setBackground(element);
+    }
+  }
+
+  public destroy(): void {
+    this.processEntriesBindInstance = null;
   }
 
   public initialise(): void {
-    const initialiseStyleBackgroundIntersectionObserver = () => {
-      const lazyBackgrounds: Element[] = Array.from(document.querySelectorAll('[style*=\'--background-image-lazy\']'));
-
-      if (lazyBackgrounds.length === 0) {
-        return;
-      }
-
-      const loadBackgroundIfElementOnScreen = (entry: IntersectionObserverEntry): void => {
-        if (entry.isIntersecting === false) {
-          return;
-        }
-
-        const target: HTMLElement = (entry.target as HTMLElement);
-        const backgroundImageLazy: string = globalThis.getComputedStyle(target).getPropertyValue('--background-image-lazy');
-
-        if (backgroundImageLazy.length === 0) {
-          return;
-        }
-
-        target.style.backgroundImage = `url('${backgroundImageLazy.replace(/(?:\\(.))/g, '$1').trim()}')`;
-        target.style.removeProperty('--background-image-lazy');
-        this.lazyBackgroundObserver!.unobserve(target);
-      };
-
-      const observeElementVisibility = (lazyBackground: Element): void => {
-        this.lazyBackgroundObserver!.observe(lazyBackground);
-      };
-
-      const setBackground = (element: Element): void => {
-        const backgroundImageLazy = globalThis.getComputedStyle(element).getPropertyValue('--background-image-lazy');
-
-        if (backgroundImageLazy === null) {
-          return;
-        }
-
-        (element as HTMLElement).style.backgroundImage = `url('${backgroundImageLazy.replace(/(?:\\(.))/g, '$1').trim()}')`;
-        (element as HTMLElement).style.removeProperty('--background-image-lazy');
-      };
-
-      if (this.isIntersectionObserverSupported) {
-        this.lazyBackgroundObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]): void => {
-          entries.forEach(loadBackgroundIfElementOnScreen);
-        });
-        lazyBackgrounds.forEach(observeElementVisibility);
-
-        return;
-      }
-
-      lazyBackgrounds.forEach(setBackground);
-    };
-
-    if (typeof document.readyState === 'string' && document.readyState === 'complete') {
-      initialiseStyleBackgroundIntersectionObserver();
+    if (typeof document.readyState === 'string' && document.readyState !== 'loading') {
+      this.initialiseStyleBackgroundIntersectionObserver();
     } else {
-      document.addEventListener('DOMContentLoaded', initialiseStyleBackgroundIntersectionObserver, true);
+      window.addEventListener('DOMContentLoaded', this.initialiseStyleBackgroundIntersectionObserver, true);
     }
   }
 }
